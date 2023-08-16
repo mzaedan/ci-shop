fcf97cf2f2e895e6345a5405f2669a2869304b3c
mzaedan <zaedanm108@gmail.com>
2023-08-16 15:56:44 +0700

    update

--- application/views/layouts/_navbar.php
+++ application/views/layouts/_navbar.php
@@ -1,8 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
 	<div class="container">
 		<a class="navbar-brand" href="<?= base_url() ?>">CIShop</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
-			aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse" id="navbarCollapse">
@@ -10,16 +9,25 @@
 				<li class="nav-item active">
 					<a class="nav-link" href="<?= base_url() ?>">Home <span class="sr-only">(current)</span></a>
 				</li>
-				<li class="nav-item dropdown">
-					<a href="#" class="nav-link dropdown-toggle" id="dropdown-1" , data-toggle="dropdown"
-						aria-haspopup="true" aria-expanded="false">Manage</a>
-					<div class="dropdown-menu" aria-labelledby="dropdown-1">
-						<a href="<?= base_url('category') ?>" class="dropdown-item">Kategori</a>
-						<a href="<?= base_url('product') ?>" class="dropdown-item">Produk</a>
-						<a href="<?= base_url('order') ?>" class="dropdown-item">Order</a>
-						<a href="<?= base_url('user') ?>" class="dropdown-item">Pengguna</a>
-					</div>
-				</li>
+				<?php
+				if ($this->session->userdata('role')) {
+					$query = $this->db->get('user');
+					$result = $query->row();
+					if ($result->name == 'Admin' && $result->name != 'member') {
+				?>
+						<li class="nav-item dropdown">
+							<a href="#" class="nav-link dropdown-toggle" id="dropdown-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Manage</a>
+							<div class="dropdown-menu" aria-labelledby="dropdown-1">
+								<a href="<?= base_url('category') ?>" class="dropdown-item">Category</a>
+								<a href="<?= base_url('product') ?>" class="dropdown-item">Product</a>
+								<a href="<?= base_url('order') ?>" class="dropdown-item">Order</a>
+								<a href="<?= base_url('user') ?>" class="dropdown-item">User</a>
+							</div>
+						</li>
+				<?php
+					}
+				}
+				?>
 			</ul>
 			<ul class="navbar-nav">
 				<li class="nav-item">
@@ -34,8 +42,7 @@
 					</li>
 				<?php else : ?>
 					<li class="nav-item dropdown">
-						<a href="#" class="nav-link dropdown-toggle" id="dropdown-2" , data-toggle="dropdown"
-							aria-haspopup="true" aria-expanded="false"><?= $this->session->userdata("name") ?></a>
+						<a href="#" class="nav-link dropdown-toggle" id="dropdown-2" , data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?= $this->session->userdata("name") ?></a>
 						<div class="dropdown-menu" aria-labelledby="dropdown-2">
 							<a href="<?= base_url('profile') ?>" class="dropdown-item">Profile</a>
 							<a href="<?= base_url('myorder') ?>" class="dropdown-item">Orders</a>
